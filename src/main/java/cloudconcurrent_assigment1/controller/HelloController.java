dd50bab95973636b4fa9dcdf2a645d4d8c1c5363
bachie07 <giabach0704@gmail.com>
2026-09-06 13:02:13 +0930

    Add /api/transcribe endpoint to receive audio, restructure controllers into controller package

--- src/main/java/cloudconcurrent_assigment1/controller/HelloController.java
+++ src/main/java/cloudconcurrent_assigment1/controller/HelloController.java
@@ -1,4 +1,4 @@
-package cloudconcurrent_assigment1;
+package cloudconcurrent_assigment1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/cloudconcurrent_assigment1/controller/TranscriptionController.java
@@ -0,0 +1,26 @@
+package cloudconcurrent_assigment1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@RestController
+public class TranscriptionController {
+	
+	private final String openAiAPIKey;
+	
+	
+	public TranscriptionController(@Value("${OPENAI_API_KEY") String openAiAPIKey) {
+		this.openAiAPIKey = openAiAPIKey;
+	}
+	
+	@PostMapping("/api/transcribe")
+	public String transcribe(@RequestParam("audio") MultipartFile audio) {
+		System.out.println("Received file: " + audio.getOriginalFilename() + ", size: " + audio.getSize() + " bytes");
+		return "received";
+	}
+
+}
