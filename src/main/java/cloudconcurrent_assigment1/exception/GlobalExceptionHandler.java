fd99559d3957c6dad523e92f98e22347d07af629
bachie07 <giabach0704@gmail.com>
2026-09-09 12:31:06 +0930

    implement global exception handler returning the 500 internal server repsonse following the specs

--- src/main/java/cloudconcurrent_assigment1/exception/GlobalExceptionHandler.java
+++ src/main/java/cloudconcurrent_assigment1/exception/GlobalExceptionHandler.java
@@ -1,5 +1,33 @@
 package cloudconcurrent_assigment1.exception;
 
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.util.Map;
+
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String, Object>> handleException(Exception ex, HttpServletRequest request){
+		
+		Map<String, Object> body = Map.of(
+				
+				"timestamp", Instant.now().toString(),
+				"status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+				"error", "Internal Server Error",
+				"message", ex.getMessage(),
+				"path", request.getRequestURI()
+				
+				);
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+				
+	} 
 
 }
